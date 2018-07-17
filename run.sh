d247d2c015def8a150930028ada430cd1729f43d
sag129 <sag129@gmail.com>
2018-07-17 17:06:18 -0400

    corrected run.sh
    changed python document name _ -> -

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+# chmod a+rx ./run.sh
+# check which python
+# which python
+# echo $PWD
+chmod +x ./run.sh
 
+python ./src/pharmacy-counting.py ./input/itcont.txt ./output/top_cost_drug.txt
