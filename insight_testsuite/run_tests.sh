41fe4741bf0f350d52b49ed01e6e7b44c17feabd
sag129 <sag129@gmail.com>
2018-07-19 12:09:57 -0400

    creating correct output files for testing

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -42,8 +42,6 @@ else
   mv ./tests/test_1/output/top_cost_drug_correct.txt ./tests/test_1/output/top_cost_drug.txt
 fi
 
-#### ^ there is some way to modularize this code into a function, but not enough time for me to do this
-
 # copy correct output file
 cp ./tests/test_2/output/top_cost_drug.txt ./tests/test_2/output/top_cost_drug_correct.txt
 
@@ -84,13 +82,13 @@ fi
 ####
 
 # copy correct output file
-cp ./tests/test_3/output/top_cost_drug.txt ./tests/test_3/output/top_cost_drug_correct.txt
+cp ./tests/test_3/output/error.txt ./tests/test_3/output/error_correct.txt
 
 # run python program
-python ../src/pharmacy-counting.py ./tests/test_3/input/itcont.txt ./tests/test_3/output/top_cost_drug.txt
+python ../src/pharmacy-counting.py ./tests/test_3/input/itcont.txt ./tests/test_3/output/top_cost_drug.txt &> ./tests/test_3/output/error.txt
 
 # check for differences between output and correct output
-diff -q ./tests/test_3/output/top_cost_drug.txt ./tests/test_3/output/top_cost_drug_correct.txt 1>/dev/null
+diff -q ./tests/test_3/output/error.txt ./tests/test_3/output/error_correct.txt 1>/dev/null
 if [[ $? == "0" ]]
 then
   # show a message for passing test
@@ -103,7 +101,7 @@ then
   echo $MSG
 
   # remove correct output file since it is the same as the output file
-  rm ./tests/test_3/output/top_cost_drug_correct.txt
+  rm ./tests/test_3/output/error_correct.txt
 else
   # show a message for failing test
   echo "[FAIL]: test_3"
@@ -115,9 +113,112 @@ else
   echo $MSG
 
   # remove bad output file
-  rm ./tests/test_3/output/top_cost_drug.txt
+  rm ./tests/test_3/output/error.txt
   # rename correct output file with original output file name
-  mv ./tests/test_3/output/top_cost_drug_correct.txt ./tests/test_3/output/top_cost_drug.txt
+  mv ./tests/test_3/output/error_correct.txt ./tests/test_3/output/error.txt
 fi
 
-python ../src/pharmacy-counting.py ./tests/test_3/input/itcont.txt ./tests/test_3/output/top_cost_drug.txt
+####
+
+# copy correct output file
+cp ./tests/test_4/output/error.txt ./tests/test_4/output/error_correct.txt
+
+# run python program
+python ../src/pharmacy-counting.py ./tests/test_4/input/itcont.txt ./tests/test_4/output/top_cost_drug.txt &> ./tests/test_4/output/error.txt
+
+# check for differences between output and correct output
+diff -q ./tests/test_4/output/error.txt ./tests/test_4/output/error_correct.txt 1>/dev/null
+if [[ $? == "0" ]]
+then
+  # show a message for passing test
+  echo "[PASS]: test_4"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="1 of 1 tests passed"
+  echo $MSG
+
+  # remove correct output file since it is the same as the output file
+  rm ./tests/test_4/output/error_correct.txt
+else
+  # show a message for failing test
+  echo "[FAIL]: test_4"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="0 of 1 tests passed"
+  echo $MSG
+
+  # remove bad output file
+  rm ./tests/test_4/output/error.txt
+  # rename correct output file with original output file name
+  mv ./tests/test_4/output/error_correct.txt ./tests/test_4/output/error.txt
+fi
+
+####
+
+# copy correct output file
+cp ./tests/test_5/output/error.txt ./tests/test_5/output/error_correct.txt
+cp ./tests/test_5/output/top_cost_drug.txt ./tests/test_5/output/top_cost_drug_correct.txt
+
+# run python program
+python ../src/pharmacy-counting.py ./tests/test_5/input/itcont.txt ./tests/test_5/output/top_cost_drug.txt &> ./tests/test_5/output/error.txt
+
+# check for differences between output and correct output
+diff -q ./tests/test_5/output/error.txt ./tests/test_5/output/error_correct.txt 1>/dev/null
+
+if [[ $? == "0" ]]
+then
+  diff -q ./tests/test_5/output/top_cost_drug.txt ./tests/test_5/output/top_cost_drug_correct.txt 1>/dev/null
+  if [[ $? == "0" ]]
+  then
+  # show a message for passing test
+  echo "[PASS]: test_5"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="1 of 1 tests passed"
+  echo $MSG
+
+  # remove correct output file since it is the same as the output file
+  rm ./tests/test_5/output/top_cost_drug_correct.txt
+  # remove correct error file since it is the same as the error file
+  rm ./tests/test_5/output/error_correct.txt
+
+  else
+  # show a message for failing test - wrong output file
+    echo "[FAIL]: test_5"
+    MSG="["
+    NOW=$(date)
+    MSG+=$NOW
+    MSG+="] "
+    MSG+="0 of 1 tests passed"
+    echo $MSG
+
+    # remove bad output file
+    rm ./tests/test_5/output/top_cost_drug.txt
+    # rename correct output file with original output file name
+    mv ./tests/test_5/output/top_cost_drug_correct.txt ./tests/test_5/output/top_cost_drug.txt
+  fi
+else
+  # show a message for failing test - wrong error message
+  echo "[FAIL]: test_5"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="0 of 1 tests passed"
+  echo $MSG
+
+  # remove bad output file
+  rm ./tests/test_5/output/error.txt
+  # rename correct output file with original output file name
+  mv ./tests/test_5/output/error_correct.txt ./tests/test_5/output/error.txt
+fi
+
+####
+
+python ../src/pharmacy-counting.py ./tests/test_6/input/itcont.txt ./tests/test_6/output/top_cost_drug.txt &> ./tests/test_6/output/error.txt
