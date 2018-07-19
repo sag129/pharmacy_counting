a322b9e0b20284b329960d50fa27301ecb4a04c8
sag129 <sag129@gmail.com>
2018-07-19 10:20:52 -0400

    test output for shell file

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -4,8 +4,41 @@
 #
 chmod +x ./run_tests.sh
 
+if command ; then
+    echo "[FAIL]: test_1"
+    echo "0 of 1 tests passed"
+else
+    echo "Command failed"
+fi
+
+cp ./tests/test_1/output/top_cost_drug.txt ./tests/test_1/output/top_cost_drug_correct.txt
+
 python ../src/pharmacy-counting.py ./tests/test_1/input/itcont.txt ./tests/test_1/output/top_cost_drug.txt
 
+diff -q ./tests/test_1/output/top_cost_drug.txt ./tests/test_1/output/top_cost_drug_correct.txt 1>/dev/null
+if [[ $? == "0" ]]
+then
+  echo "[PASS]: test_1"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="1 of 1 tests passed"
+  echo $MSG
+else
+  echo "[FAIL]: test_1"
+  MSG="["
+  NOW=$(date)
+  MSG+=$NOW
+  MSG+="] "
+  MSG+="0 of 1 tests passed"
+  echo $MSG
+fi
+
+rm ./tests/test_1/output/top_cost_drug_correct.txt
+
 python ../src/pharmacy-counting.py ./tests/your-own-test_1/input/itcont.txt ./tests/your-own-test_1/output/top_cost_drug.txt
+
+
 
 python ../src/pharmacy-counting.py ./tests/your-own-test_1/input/itcont1.txt ./tests/your-own-test_1/output/top_cost_drug1.txt
